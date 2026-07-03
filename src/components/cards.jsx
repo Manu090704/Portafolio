fb968bae0b00d3fc7e04df0a3c565c02db65905a
Manuel Bajos <mbajosr@hotmail.com>
2026-07-03 16:02:31 -0600

    fix: se exporta la tarjeta

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,18 +1,22 @@
-import chip from './chip.jsx';
+import Chip from './chip.jsx';
 
-const Cards = ({title, subtitle, description, link, tecnologies}) => {
-
-    return (
-        <>
-        <div className="card">
-            <h3>{title}</h3>
-            <h4>{subtitle}</h4>
-            <p>{description}</p>
-            <a href={link} target="_blank">View Project</a>
-            <div className="tecnologies">
-                {tecnologies.map((tecnology, index)=>(<Chip text={tecnology} key={index} />))}
-            </div>
+const Cards = ({ title, subtitle, description, link, tecnologies, children }) => {
+  return (
+    <>
+      <div className="">
+        <h3>{title}</h3>
+        <h4>{subtitle}</h4>
+        <p>{description}</p>
+        <a href={link} target="_blank">View Project</a>
+        <div className="tecnologies">
+          {tecnologies.map((technology, index) => (
+            <Chip text={technology} key={index} />
+          ))}
         </div>
-        </>
-    )
+      </div>
+        {children}
+    </>
+  );
 };
+
+export default Cards;
