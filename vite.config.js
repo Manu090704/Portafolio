b1a112ef9dd5018f9dbe80cdba14fcfcef006f32
Manuel Bajos <mbajosr@hotmail.com>
2026-07-03 18:18:01 -0600

    feat: se añade deploy.yml para desplegar el portafolio

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'Portafolio'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/',
 })
